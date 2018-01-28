66e52e0445a09bb3bdaa7b6b9d3885fd744116ba
lennertz <lennertz@uark.edu>
2018-01-28 10:22:57 -0600

    Add files via upload

---
+++ run_analysis.R
@@ -0,0 +1,56 @@
+#load needed libraries
+library(dplyr)
+library(knitr)
+#download and unzip files
+if(!file.exists("./data")){dir.create("./data")}
+fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+download.file(fileUrl,destfile="./data/Dataset.zip")
+
+unzip(zipfile="./data/Dataset.zip",exdir="./data")
+path_rf <- file.path("./data" , "UCI HAR Dataset")
+files<-list.files(path_rf, recursive=TRUE)
+files
+#load activity files, subject files, 
+dataActivityTest  <- read.table(file.path(path_rf, "test" , "Y_test.txt" ),header = FALSE)
+dataActivityTrain <- read.table(file.path(path_rf, "train", "Y_train.txt"),header = FALSE)
+dataSubjectTrain <- read.table(file.path(path_rf, "train", "subject_train.txt"),header = FALSE)
+dataSubjectTest  <- read.table(file.path(path_rf, "test" , "subject_test.txt"),header = FALSE)
+dataFeaturesTest  <- read.table(file.path(path_rf, "test" , "X_test.txt" ),header = FALSE)
+dataFeaturesTrain <- read.table(file.path(path_rf, "train", "X_train.txt"),header = FALSE)
+#1. Merging to create 1 dataset
+#combine Train and test sets
+dataSubject <- rbind(dataSubjectTrain, dataSubjectTest)
+dataActivity<- rbind(dataActivityTrain, dataActivityTest)
+dataFeatures<- rbind(dataFeaturesTrain, dataFeaturesTest)
+#providing names for the variables
+names(dataSubject)<-c("subject")
+names(dataActivity)<- c("activity")
+dataFeaturesNames <- read.table(file.path(path_rf, "features.txt"),head=FALSE)
+#Make the dataframe
+names(dataFeatures) <- dataFeaturesNames$V2
+dataCombine <- cbind(dataSubject, dataActivity)
+Data <- cbind(dataFeatures, dataCombine)
+#2. Extracts only the measurements on the mean and standard deviation for each measurement.
+subdataFeaturesNames <- dataFeaturesNames$V2[grep("mean\\(\\)|std\\(\\)", dataFeaturesNames$V2)]
+selectedNames <- c(as.character(subdataFeaturesNames), "subject", "activity")
+Data <- subset(Data,select=selectedNames)
+#3. Uses descriptive activity names to name the activities in the data set
+activityLabels <- read.table(file.path(path_rf, "activity_labels.txt"),header = FALSE)
+Data$activity <- factor(Data$activity, labels = as.character(activityLabels[,2]))
+#4 Appropriately Label dataset - prefix t is replaced by time,Acc is replaced by Accelerometer,Gyro is replaced by Gyroscope,prefix f is replaced by frequency,Mag is replaced by Magnitude
+#BodyBody is replaced by Body
+names(Data) <- gsub("^t", "time", names(Data))
+names(Data) <- gsub("^f", "frequency", names(Data))
+names(Data) <- gsub("Acc", "Accelerometer", names(Data))
+names(Data) <- gsub("Gyro", "Gyroscope", names(Data))
+names(Data) <- gsub("Mag", "Magnitude", names(Data))
+names(Data) <- gsub("BodyBody", "Body", names(Data))
+# Function to create the data set file based on the clean data set tablelibrary.
+
+Data2<-aggregate(. ~subject + activity, Data, mean)
+Data2<-Data2[order(Data2$subject,Data2$activity),]
+write.table(Data2, file = "tidydata.txt",row.name=FALSE)
+
+
+
+
